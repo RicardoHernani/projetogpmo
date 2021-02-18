66fe5f07cefb84c9da687209a25ae4b18c6b84cb
Ricardo Hernani <ricardohernani@yahoo.com.br>
2021-02-17 21:08:48 -0300

    Banco de dados H2 e criação automatica da base de dados

--- src/main/java/com/ricardohernani/projetogpmo/domain/Paciente.java
+++ src/main/java/com/ricardohernani/projetogpmo/domain/Paciente.java
@@ -3,10 +3,19 @@ package com.ricardohernani.projetogpmo.domain;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Paciente implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
 	private Integer registro;
 	private Date data;
 	
