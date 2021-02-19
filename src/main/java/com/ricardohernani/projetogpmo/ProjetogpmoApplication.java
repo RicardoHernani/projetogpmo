a5b4b2b99431981205fc462b9020b959d8275d19
Ricardo Hernani <ricardohernani@yahoo.com.br>
2021-02-18 22:08:19 -0300

    Criando instanciação de dois pacientes

--- src/main/java/com/ricardohernani/projetogpmo/ProjetogpmoApplication.java
+++ src/main/java/com/ricardohernani/projetogpmo/ProjetogpmoApplication.java
@@ -1,13 +1,35 @@
 package com.ricardohernani.projetogpmo;
 
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetogpmoApplication {
+import com.ricardohernani.projetogpmo.domain.Paciente;
+import com.ricardohernani.projetogpmo.repositories.PacienteRepository;
 
+@SpringBootApplication
+public class ProjetogpmoApplication implements CommandLineRunner {
+
+	@Autowired
+	private PacienteRepository pacienteRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetogpmoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Paciente pcte1 = new Paciente(null, 32144881, new java.util.Date(sdf.parse("29/12/2020").getTime()));
+		Paciente pcte2 = new Paciente(null, 12345678, new java.util.Date(sdf.parse("23/01/2021").getTime()));
+		
+		pacienteRepository.saveAll(Arrays.asList(pcte1, pcte2));
 	}
 
 }
--- src/main/java/com/ricardohernani/projetogpmo/resources/PacienteResource.java
+++ src/main/java/com/ricardohernani/projetogpmo/resources/PacienteResource.java
@@ -1,8 +1,5 @@
 package com.ricardohernani.projetogpmo.resources;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,13 +14,13 @@ import com.ricardohernani.projetogpmo.services.PacienteService;
 @RequestMapping(value="/pacientes")
 public class PacienteResource {
 
-	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
 	
 	@Autowired
 	private PacienteService service;
 	
 	@RequestMapping (value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) throws ParseException {
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Paciente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
