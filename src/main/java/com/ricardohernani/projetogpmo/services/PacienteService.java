48d0fa1c77d674c66f152a6f9284143d38a16597
Ricardo Hernani <ricardohernani@yahoo.com.br>
2021-02-21 23:31:12 -0300

    Tratamento para objeto não encontrado em Paciente

--- src/main/java/com/ricardohernani/projetogpmo/services/PacienteService.java
+++ src/main/java/com/ricardohernani/projetogpmo/services/PacienteService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.ricardohernani.projetogpmo.domain.Paciente;
 import com.ricardohernani.projetogpmo.repositories.PacienteRepository;
+import com.ricardohernani.projetogpmo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class PacienteService {
@@ -16,7 +17,8 @@ public class PacienteService {
 
 	public Paciente find(Integer id) {
 		Optional<Paciente> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+						"Objeto não encontrado! Id: " + id + ", Tipo: " + Paciente.class.getName()));
 	}
 	
 }
